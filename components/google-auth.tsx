62a5911bcf20d6b6da5a53b68f70b01985d6b028
‘lordieXXL’ <caramorton40@gmail.com>
2024-08-22 15:44:35 +0100

    refactoring

---
+++ components/google-auth.tsx
@@ -0,0 +1,55 @@
+import { useOAuth } from "@clerk/clerk-expo";
+import React from "react";
+import * as Linking from "expo-linking";
+import { Text, TouchableOpacity } from "./Themed";
+import { Image, StyleSheet } from "react-native";
+
+const GoogleAuth = ({ title }: { title: string }) => {
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+  const handleOAuth = React.useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow({
+          redirectUrl: Linking.createURL("/dashboard", { scheme: "myapp" }),
+        });
+
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId });
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
+
+  return (
+    <TouchableOpacity onPress={handleOAuth} style={styles.container}>
+      <Image
+        style={{
+          height: 20,
+          width: 20,
+        }}
+        source={require("../assets/icons/google.png")}
+      />
+      <Text>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default GoogleAuth;
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 10,
+    gap: 10,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: "#565454",
+    padding: 10,
+    borderRadius: 8,
+  },
+});
